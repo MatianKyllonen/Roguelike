35fed0b6da55741da8ad32842100e0671ff6df4d
Matian Kyllönen <118250414+MatianKyllonen@users.noreply.github.com>
2024-12-05 13:52:55 +0200

    5.12.2024
    Oi maamme suomi synnyin maa

--- RogueLike/Assets/Scripts/Bullet.cs
+++ RogueLike/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     public int damage = 50;
     public int playerNumber; // Track which player fired this bullet
     public Movement player;
+    public GameObject lifeStealEffect;
     public float lifeStealChance = 0f;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,18 +29,29 @@ public class Bullet : MonoBehaviour
                 {
                     Gamemanager.instance.UpdatePlayerStats(playerNumber, 1, 0, 0, 0); 
                 }
+
                 if(lifeStealChance > 0)
                 {
                     if(Random.Range(0, 100) < lifeStealChance)
                     {
+                        if(lifeStealEffect != null)
+                            Instantiate(lifeStealEffect, transform.position, Quaternion.identity);
+
                         int healingAmount = Mathf.RoundToInt(damage / 5);
                         player.Heal(healingAmount);
                         Gamemanager.instance.UpdatePlayerStats(playerNumber, 0, 0, 0, healingAmount); 
+                        GetComponent<BoxCollider2D>().enabled = false;
+                        GetComponent<SpriteRenderer>().enabled = false;
                     }
-                }
+
+                }         
             }
 
-            Destroy(gameObject); // Destroy bullet
-        }
+            Destroy(gameObject);
+
+        }    
     }
+
+
+
 }
